dbdb18208744349a9fbbe457ff98096323fb100a
Bessie Lee <chienlinglee@gmail.com>
2025-12-14 15:22:12 +0000

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Upload from "./pages/Upload";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Upload />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
