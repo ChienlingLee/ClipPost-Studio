9a8ffe4511b2d2d42f1d3e6cceef615d564106e3
Bessie Lee <chienlinglee@gmail.com>
2025-12-16 10:10:23 +0000

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Uploader from "./Uploader";
+import Uploader from "./components/Uploader";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
